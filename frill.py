ea33c7542156abb1bdd418213234191987e5f1c5
Vlad <30626779+belliavesha@users.noreply.github.com>
2019-03-21 01:31:26 +0200

    update frill
    updated python method

--- frill.py
+++ frill.py
@@ -1,10 +1,9 @@
-from ethercalc import compute
 from matplotlib.pyplot import *
 from numpy import *
 from random import random as rnd
 from matplotlib.widgets import Slider, TextBox, Button
 import matplotlib.text as txt
-
+ 
 fig, ax = subplots()
 ax.set_title( 'F(s)' )
 subplots_adjust(left=0.25, bottom=0.25) 
@@ -23,30 +22,31 @@ slaC =  Slider(axaC, r'$\varphi_s$', -pi, pi, valinit=0)
 slrC =  Slider(axrC, r'$\rho_s$', 0, 2, valinit=1)
 slN =  Slider(axN, 'N', 1., 3, valinit=2)
 
-def draw(N,aC,rC):
+def draw(N,aC,rC):         
 
-    x_f, y_f, n_xy = compute.frill(N, aC, rC) 
-    # print(frill.compute.__doc__)
-    x = x_f[:n_xy-1]; y = y_f[:n_xy-1]
-    # sx = rC*cos(aC)
-    # sy = rC*sin(aC)
-
-    # x = []
-    # y = []
-
-    # for k in range(N):
-    #     rho = k/N
-    #     sa = 1+rho*rho
-    #     sb = 1-rho*rho
-    #     sc = sqrt(sa*sa-sb*sb)
-    #     dif = 10
-    #     pha = 0
-    #     for j in range(N):
-    #         phi = j*2*pi/N
-    #         sa2 = sqrt((sc*cos(phi) - sx)**2 + (sc*sin(phi) - sy)**2)  + sqrt((sc*cos(phi) + sx)**2 + (sc*sin(phi) + sy)**2)
-    #         if abs(sa2-2*sa)<1/4/N:
-    #             x.append(rho*cos(phi))
-    #             y.append(rho*sin(phi))
+    try:
+        from ethercalc import compute
+        x_f, y_f, n_xy = compute.frill(N, aC, rC) 
+        # print(frill.compute.__doc__)
+        x = x_f[:n_xy-1]; y = y_f[:n_xy-1]
+    except:
+        x =[]
+        y =[]
+        for j in range(N):
+            sy = rC*(j*2/N-1+1/N)
+            sx = sqrt(rC*rC-sy*sy)
+            phi = arcsin(j*2/N-1+1/N)
+            r = 1 - rC*rC
+            q = 2*sx*sx- 2*sy*sy
+            p = r-3
+            P = poly1d([1,0,p,q,r])
+            for c in P.r:
+                if abs(c.imag)==0 and 0<=c.real<=1:
+                    rho = sqrt(c.real)
+                    x.append(rho*cos(aC+phi))
+                    y.append(rho*sin(aC+phi))
+                    x.append(-rho*cos(aC+phi))
+                    y.append(-rho*sin(aC+phi))
 
     ll.set_data(x,y)
     fig.canvas.draw_idle()
@@ -58,8 +58,10 @@ def update(val):
     N = int(exp(slN.val*log(1e1)))
     draw(N,aC,rC)
 
+update(0)
 slrC.on_changed(update)
 slaC.on_changed(update)
 slN.on_changed(update)
 show()
+
 
