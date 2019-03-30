5f37a478db6979c080cb2b61e54e17eef3a29c5c
Vlad <30626779+belliavesha@users.noreply.github.com>
2019-03-30 09:43:27 +0200

    petal update
    false positive periods in consideration

--- petal.py
+++ petal.py
@@ -12,6 +12,7 @@ N = a.shape[-1]
 na = a - sum(a)/N
 sp = fft.fft(na)[:N//2]
 pf = fft.fftfreq(N)[:N//2]
+# sp = sp+sp[::-1]
 asp = abs(sp)
 
 def profile(x, A, M, S):
@@ -19,71 +20,85 @@ def profile(x, A, M, S):
     return A/(1+(x-M)**2/S**2)
 
 def fp(asp):
-    mi = 0  
-    mx = 0
+
+    mix = []
+    masp = amax(asp)
     for i in range(len(asp)):
-        if asp[i]> mx:
-            mx = asp[i]
-            mi = i
-    casp = asp[mi-5:mi+5]
-    cupf = pf[mi-5:mi+5]
-
-    p = crvfit(profile, cupf, casp, [mx, pf[mi], pf[mi+1]-pf[mi-1]] )[0]
-    cupft=linspace(cupf[0],cupf[-1],200)
-    plot(cupft,profile(cupft,p[0],p[1],p[2]),'b-')
-    plot(cupf,casp,'r.')
-    plot(pf,sp.real,'-')
-    plot(pf,sp.imag,'-')
-    show()
-    return 1/p[1]
+        if asp[i]> masp/2:
+            mix.append((i,asp[i]))
 
 
+    P = []
+    for  mi,mx in mix:
+        casp = asp[mi-5:mi+5]
+        cupf = pf[mi-5:mi+5]
+        p = crvfit(profile, cupf, casp, [mx, pf[mi], pf[mi+1]-pf[mi-1]] )[0]
+        cupft=linspace(cupf[0],cupf[-1],200)
+        P.append(abs(1/p[1]))
+    #     plot(cupft,profile(cupft,p[0],p[1],p[2]),'b-')
+    #     plot(cupf,casp,'r.')
+
+    # plot(pf,asp,'-')
+    # plot(pf,sp.imag,'-')
+    # show()
+    return P
 
 
-P= fp(asp)
-print(P)
-# P = 1/0.29772
-# print(P)
-iph = sort(array([ (i,(1.*i)%P) for i in range(int(3*sqrt(N)))], dtype=[('ind', '<i4'),('phase', '<f8')] ),order='phase')
-# print(inds)
-
-ona = a[iph['ind']]
 
 
-L = 0
-w = []
-l = []
-pik = 0
-for j in range(-2,len(iph)):
-    l.append(L)
-    i, ph = iph[j-1]
-    i1, ph1 = iph[j]
-    v = a[i1]-a[i]
-    av = abs(v)
-    L += abs(v)
-    phase = log(v/av)
-    phase = phase.imag%(2*pi)+pik*2*pi
-    dp = phase - w[-1] if w else 0 
-    if dp>4:
-        pik-=1
-        phase -= 2*pi
-    if dp<-4:
-        pik+=1
-        phase += 2*pi
-    w.append(phase)
+Ps= fp(asp)
+# print(Ps)
+rL = 0
+liph = int(sqrt(N)*1.5)
+for P in Ps:
+    iph = sort(array([ (i,(1.*i)%P) for i in range(liph)], dtype=[('ind', '<i4'),('phase', '<f8')] ),order='phase')
 
-w = array(w)
+    L = 0
+    pik = 0
+    l = []
+    w = []
+    # r = []
+    for j in range(-2,len(iph)):
+        l.append(L)
+        i, ph = iph[j-1]
+        i1, ph1 = iph[j]
+        v = a[i1]-a[i]
+        # r.append(abs(a[i1]))
+        av = abs(v)
+        L += abs(v)
+        phase = log(v/av)
+        phase = phase.imag%(2*pi)+pik*2*pi
+        dp = phase - w[-1] if w else 0 
+        if dp>4:
+            pik-=1
+            phase -= 2*pi
+        if dp<-4:
+            pik+=1
+            phase += 2*pi
+        w.append(phase)
+    if 1>rL*L:
+        rL = 1/L
+        ona = a[iph['ind']]
+        oma = array(w)
+        lie = array(l)
+        # rad = array(r)
 
 figure(1)
-title("Length = "+str(L))
+title("Length = "+str(1/rL))
 plot(ona.real,ona.imag,'-')
-for j in range(-len(iph)//20,len(iph),len(iph)//10):
+plot([0],[0],'r.', markersize=10.)
+for j in range(-2,liph,liph//10):
     plot(ona[j].real, ona[j].imag,'.')
 axis('scaled')
 
 figure(2)
-plot(l, w)
-for j in range(-len(iph)//20,len(iph),len(iph)//10):
-    plot(l[j], w[j],'.')
+plot(lie, oma)
+for j in range(0,liph,liph//10):
+    plot(lie[j], oma[j],'.')
+
+# figure(3)
+# plot(lie, rad)
+# for j in range(0,liph,liph//10):
+#     plot(lie[j], rad[j],'.')
+
 show()
-# 
