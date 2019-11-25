f51118cce53187dad00d21cc7b50c0dd2f2fa5e2
dmitrievdv <dmitrievdv242@gmail.com>
2019-11-25 22:02:11 +0300

    Petal calculation completed

--- ether.jl
+++ ether.jl
@@ -1,6 +1,7 @@
 module Ether
 
 using Polynomials
+using Plots
 
 function chebsecondkind(n::Int)
 	U₀ = Poly([1])
@@ -85,7 +86,7 @@ function expfₙ(n::Int, a, b)
 	elseif n > 0
 		return 1/2*fₙ(n, a, b)
 	elseif n < 0 
-		return conj(1/2*fₙ(-n, a, b))
+		return 1/2*fₙ(-n, a, b)
 	end
 end
 
@@ -104,14 +105,32 @@ end
 function calc_g(m::Int, a, b)
 	function g(x)
 		s = 0
-		for n=-m:m
-			if(n != 0)
-				s += expfₙ(n, a, b)*(exp(-1.0im*n*x) - sin(n*x + n/2.0)/sin(n/2.0))*exp(-1.0im*n*x)
-			end
+		for n=1:m
+			# print(expfₙ(n, a, b)*(exp(-1.0im*n*x) - sin(n*x + n/2.0)/sin(n/2.0))*exp(-1.0im*n*x), "\n")
+			s += expfₙ(n, a, b)*(exp(-1.0im*n*x) - sin(n*x + n/2.0)/sin(n/2.0))*exp(-1.0im*n*x)
+			s += expfₙ(n, a, b)*(exp(1.0im*n*x) - sin(n*x + n/2.0)/sin(n/2.0))*exp(1.0im*n*x)
+			# print(n, " ", expfₙ(n, a, b), " ", s, "\n")
 		end
 		return s
 	end 
 	return g
+end
+
+function calc_petal(t, φ; n = 10)
+	areal = cos(φ) * (1+t^2)
+	aimag = sin(φ) * (1-t^2)
+	a = areal + aimag*1.0im
+	# print(a)
+	return calc_g(n, a, 2*t)
+end
+
+function plot_petal(t, φ; nx = 400)
+	function petal(n); calc_petal(t, φ; n = n); end
+	x = [0:π/nx:π;]
+	pyplot(size = (700, 500))
+	plot(real.(exp.(petal(10).(x))), imag.(exp.(petal(10).(x))), aspect_ratio="equal", label = "10")
+	plot!(real.(exp.(petal(20).(x))), imag.(exp.(petal(20).(x))), aspect_ratio="equal", label = "20")
+	plot!(real.(exp.(petal(30).(x))), imag.(exp.(petal(30).(x))), aspect_ratio="equal", label = "30")
 end
 
 # function get_next(an :: Number, n :: Int)
